c919a67946fd824937768f60e8ecfab482d4dfae
shivakarka <shivakarka@gmail.com>
2022-12-07 23:48:21 +0530

    small fixes in mobile view

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,7 +7,7 @@ function Contact() {
   return (
     <div
       name="contact"
-      className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4"
+      className="w-full h-full sm:h-screen bg-[#0a192f] flex justify-center items-center p-4"
     >
       <form
         method="POST"
